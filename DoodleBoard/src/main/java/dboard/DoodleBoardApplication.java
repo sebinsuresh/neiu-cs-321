d85ad28efb3d60e00ffd8a3b3fcff5fc951061c9
Sebin <sebinsuresh@gmail.com>
2020-09-07 23:57:13 -0500

    Fixed directory structure. New Project first commit from IntelliJ.

---
+++ DoodleBoard/src/main/java/dboard/DoodleBoardApplication.java
@@ -0,0 +1,13 @@
+package dboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoodleBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoodleBoardApplication.class, args);
+    }
+
+}
---
+++ DoodleBoard/src/test/java/dboard/DoodleBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package dboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoodleBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
