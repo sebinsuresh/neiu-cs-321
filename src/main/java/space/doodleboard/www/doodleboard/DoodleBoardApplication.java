d79fee812a04e6a6c094a43be787b7e76c342422
sebinsuresh <sebinsuresh@gmail.com>
2020-09-07 23:36:50 -0500

    First commit from IntelliJ

---
+++ src/main/java/space/doodleboard/www/doodleboard/DoodleBoardApplication.java
@@ -0,0 +1,13 @@
+package space.doodleboard.www.doodleboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoodleBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoodleBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/space/doodleboard/www/doodleboard/DoodleBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package space.doodleboard.www.doodleboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoodleBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
