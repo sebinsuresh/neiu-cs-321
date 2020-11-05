4ac236355471d2c70461a7781f70f5b90804754c
Sebin <sebinsuresh@gmail.com>
2020-11-05 01:45:36 -0600

    Week11 HW9: Implemented touch drawing and responsive resizing of canvas.
    Scroll by touch only occurs outside of canvas/on the side edges. Line algorithm for connecting missing touch/mouse data implemented. Resizing canvas dynamically by comparing the parent element's resolution.

--- DoodleBoard/src/main/resources/static/scripts/draw_script.js
+++ DoodleBoard/src/main/resources/static/scripts/draw_script.js
@@ -1,23 +1,33 @@
-let palette;
-let drawing;
-let drawStartX = 30, drawStartY=30;
-let pxSize = 10;
+let palette;    // array
+let drawing;    // 2d array of strings of hex colors
 let currCol;
 let isDrawing = false;
 let drawStrElem;
+let swatchSize = 50;
+let drawStartX = 5, drawStartY=5;
+let lastCanW = 0, lastCanH = 0;
+let pxSize = 10;
+let parentDiv;
+let drwRegionW;
+let smallerUI = false;
 
 // P5js' setup() function will occur once before the first draw() call occurs
 function setup() {
+    parentDiv = select("#p5CanvasDiv");
+    cnv = createCanvas(lastCanW, lastCanH);
+    calcUI(parentDiv);
+
+    cnv.style("touch-action: none;");   // prevent scrolling in mobile
+
     // Set the div #p5CanvasDiv as the parent to the canvas created by p5js
-    cnv = createCanvas(700, 800);
-    cnv.parent('p5CanvasDiv');
+    cnv.parent(parentDiv);
+
     textSize(16);
-    textFont('consolas');
+    textFont('consolas, courier new');
 
     // Get palette hex values in an array
     palette = document.getElementById("plt").value;
     palette = palette.substring(1, palette.length-1).split(", ");
-    // console.log(palette);
 
     drawStrElem = document.getElementById("drawingStr");
     let initialDrawing = drawStrElem.value;
@@ -38,18 +48,23 @@ function setup() {
 
 // P5js' draw() function will iterate 60 times per second.
 function draw() {
+    calcUI(parentDiv);
     background(255);
 
     noStroke();
     fill(palette[0]);
 
-    text('◀ ▶ Press L/R arrow keys to switch colors.', drawStartX, height - 50);
-    text('🖱   Left click to draw, Right Click to Erase.', drawStartX, height - 30);
-    text('🔄  Refresh to reset.', drawStartX, height - 10);
+    if(!smallerUI){
+        text('◀ ▶ L/R arrow keys to switch colors.', drawStartX, height - 50);
+        text('🖱   L/R click to Draw/Erase.', drawStartX, height - 30);
+        text('🔄  Refresh to reset.', drawStartX, height - 10);
+    } else {
+        text('🔄  Refresh to reset.', drawStartX, height - 10);
+    }
 
     stroke(0);
     noFill();
-    square(drawStartX, drawStartY, 640);
+    square(drawStartX, drawStartY, drwRegionW);
 
     // drawCheckerBoard(drawStartX, drawStartY, 640, 640);     // Draw the white & gray checkerboard pattern
 
@@ -66,6 +81,38 @@ function draw() {
     drawPalette();
     handleMouse();
     setDrawingString(); // set the drawingStr element value
+
+    // text(~~frameRate(), 20,20);  // DEBUG PERFORMANCE
+}
+
+// Calculate some of the UI related stuff - canvas size, pixel size,
+// drawing region, swatchSize, and determine if it's the smaller UI form.
+function calcUI(parentDiv){
+    if(parentDiv.size().width != lastCanW || parentDiv.size().height != lastCanH){
+        lastCanW = parentDiv.size().width;
+        lastCanH = parentDiv.size().height;
+        if(lastCanW < 365){
+            swatchSize = 40;
+            let newCanvW  = 64*~~((lastCanW-2*drawStartX)/64);
+            newCanvW += 2*drawStartX;
+            resizeCanvas(newCanvW, newCanvW+16+swatchSize+4+swatchSize+4+(25)+drawStartX);
+            smallerUI = true;
+        } else if(lastCanW < 650){
+            let newCanvW  = 64*~~((lastCanW-2*drawStartX)/64);
+            newCanvW += 2*drawStartX;
+            resizeCanvas(newCanvW, newCanvW+16+swatchSize+4+swatchSize+4+(25)+drawStartX);
+            smallerUI = true;
+        } else{
+            resizeCanvas(650, 800);
+            smallerUI = false;
+        }
+        drwRegionW = cnv.size().width - 2*drawStartX;   // subtracting the small boundaries
+        // drawing region is drwRegionW tall and wide
+
+        pxSize = ~~(drwRegionW/64); // ~~ will truncate float to an int
+        drwRegionW = 64*pxSize;     // Avoid leftover width
+
+    }
 }
 
 // Clear the canvas by filling all pixels with transparent
@@ -82,7 +129,7 @@ function clearCanvas(){
 function drawCheckerBoard(sX, sY, w, h){
     noStroke();
     let fillC = 255;
-    let tiny = ~~(pxSize/2)
+    let tiny = ~~(pxSize/2);
     for(let i = sX; i < sX + w; i += tiny){
         for(let j = sY; j < sY + h ; j += tiny){
             if(fillC == 202)
@@ -92,22 +139,29 @@ function drawCheckerBoard(sX, sY, w, h){
             fill(fillC);
             square(i, j, tiny);
         }
-        if(fillC == 202)
-            fillC = 255;
-        else
-            fillC = 202;
+        if(!smallerUI){
+            if(fillC == 202)
+                fillC = 255;
+            else
+                fillC = 202;
+        }
     }
 }
 
 // Function that draws the palette below the drawing area and handles the clicks
 function drawPalette(){
-    let swatchSize = 50;
-    let paddingSize = 3.63;
-    let startY = 680;
+    let paddingSize = 4;
+    let startY = drawStartY + drwRegionW + 16;
     for(let i = 0; i < 12; i++){
         noStroke();
         fill(palette[i]);
+
         let startX = drawStartX + i*swatchSize + i * paddingSize;
+        if(smallerUI && i > 5){
+            startY = drawStartY + drwRegionW + 16 + swatchSize + paddingSize;
+            startX = drawStartX + (i-6)*swatchSize + (i-6) * paddingSize;
+        }
+
         if(i == 11) {
             drawCheckerBoard(startX, startY, swatchSize, swatchSize);
         } else {
@@ -128,14 +182,28 @@ function drawPalette(){
     stroke('gray');
     strokeWeight(3);
     noFill();
-    square(drawStartX + currentCol * swatchSize + currentCol * paddingSize, startY, swatchSize);
+    startY = drawStartY + drwRegionW + 16;
+    if(!smallerUI || currentCol < 6){
+        square(drawStartX + currentCol * swatchSize + currentCol * paddingSize, startY, swatchSize);
+    } else {
+        if(currentCol > 5){
+            square(drawStartX + (currentCol-6) * swatchSize + (currentCol-6) * paddingSize,
+                startY + paddingSize + swatchSize, swatchSize);
+        }
+    }
+
     strokeWeight(1);
 }
 
 // Returns a vector with mouse position in drawing area
-function getMouseCanvasVec(){
-    let mX = pxSize*~~(mouseX/pxSize);
-    let mY = pxSize*~~(mouseY/pxSize);
+function getMouseCanvasVec(pmouse = false){
+    let origMX = mouseX, origMY = mouseY;
+    if(pmouse){
+        origMX=pmouseX;
+        origMY=pmouseY;
+    }
+    let mX = drawStartX + pxSize*~~(origMX/pxSize);
+    let mY = drawStartY + pxSize*~~(origMY/pxSize);
 
     if (mX < drawStartX || mX > drawStartX + (63 * pxSize) ||
         mY < drawStartY || mY > drawStartY + (63 * pxSize)){
@@ -147,6 +215,7 @@ function getMouseCanvasVec(){
 
 // Function that handles mouse clicks and movements
 function handleMouse(){
+    let pMouseCell = getMouseCanvasVec(true);
     let mouseCell = getMouseCanvasVec();
 
     if(mouseCell) {
@@ -158,13 +227,18 @@ function handleMouse(){
         fill("rgba("+ currentR + "," + currentG + "," + currentB + ", 0.7)");
         square(mouseCell.x, mouseCell.y, pxSize);
 
-        if(mouseIsPressed){
+        if(mouseIsPressed || touches.length == 1){
             isDrawing = true;
             let x = ~~((mouseCell.x-drawStartX)/pxSize);
             let y = ~~((mouseCell.y-drawStartY)/pxSize);
+
             // console.log(x + ", " + y);
-            if (mouseButton == LEFT) {
-                drawing[x][y] = palette[currentCol];
+            if (mouseButton == LEFT || touches.length > 0) {
+                if(pMouseCell){
+                    drawLine(pMouseCell.x, pMouseCell.y, mouseCell.x, mouseCell.y);
+                } else {
+                    drawing[x][y] = palette[currentCol];
+                }
             } else {
                 document.oncontextmenu = function() { return false; }   // Prevents right-click menu
                 drawing[x][y] = "#000000";
@@ -174,6 +248,47 @@ function handleMouse(){
             document.oncontextmenu = function() { return true; }    // Re-enable right-click menu
         }
     }
+}
+
+function drawLine(x1, y1, x2, y2, w=pxSize) {
+    let nDivs, xSep, ySep;
+    let absac = abs(x1 - x2),
+        absbd = abs(y1 - y2);
+    if (absac > absbd) {
+        nDivs = absac / w;
+        xSep = w;
+        ySep = (absbd / nDivs);
+    } else {
+        nDivs = absbd / w;
+        xSep = (absac / nDivs);
+        ySep = w;
+    }
+
+    if (x1 > x2)
+        xSep *= -1
+    if (y1 > y2)
+        ySep *= -1
+
+    noStroke();
+    // fill('white');
+    if(nDivs > 0){
+        for (let i = 0; i <= nDivs; i++) {
+            let nx = x1 + (w / 2) + i * xSep,
+                ny = y1 + (w / 2) + i * ySep;
+            // circle(nx, ny, 10);
+            let foundCell = findCell(nx, ny, w);
+            if(foundCell[0]<64 && foundCell[1]<64)
+                drawing[foundCell[0]][foundCell[1]] = palette[currentCol];
+            else
+                drawing[min(63,foundCell[0])][min(63,foundCell[1])] = palette[currentCol];
+        }
+    } else {
+        drawing[findCell(x1, y1, w)[0]][findCell(x1, y1, w)[1]] = palette[currentCol];
+    }
+}
+
+function findCell(px, py, w) {
+    return [~~((px-drawStartX) / w), ~~((py-drawStartY) / w)];
 }
 
 // Handle shortcuts by keyboard key presses
