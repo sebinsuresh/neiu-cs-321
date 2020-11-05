3e4fb6e3f8e6505675f14d97673c21cdf94f853f
Sebin <sebinsuresh@gmail.com>
2020-11-04 18:33:10 -0600

    Week11 HW9 (WIP): Bootstrap partially done
    Added navigation to most pages.
    Added footer to most pages.
    Added responsive resizing of canvas elements in the feed page.

--- DoodleBoard/src/main/resources/static/scripts/feed_script.js
+++ DoodleBoard/src/main/resources/static/scripts/feed_script.js
@@ -1,31 +1,46 @@
-let posts = document.getElementsByClassName("post");
+let posts = document.getElementsByClassName("feedpost");
 let palette = document.getElementById("plt").value;
 palette = palette.substring(1, palette.length-1).split(", ");
 
-document.addEventListener('DOMContentLoaded', loadPosts, false);
+document.addEventListener('DOMContentLoaded', initialize, false);
+
+function initialize(){
+    loadPosts();
+    $(window).resize(function() {
+        loadPosts();
+    })
+}
 
 function loadPosts(){
     for(let i = 0; i < posts.length; i++){
         let post = posts[i];
         let drawStr = post.children[1].value;   //hidden input element
-        let cnv = post.children[0]; // canvas element
-        cnv.width = 640;
-        cnv.height = 640;
+        let cnv = setcnv(post) // canvas element
         drawDoodle(cnv, drawStr);
     }
+}
+
+function setcnv(post){
+    let cnv = post.children[0];
+    cnv.width = ~~(posts[0].clientWidth) - 2*(parseInt($(posts[0]).css('padding-left').substring(-2)));
+    cnv.height = cnv.width;
+    return cnv;
 }
 
 
 function drawDoodle(cnv, drawStr){
     let c = cnv.getContext("2d");
+    let w = ~~(cnv.width/64);
     for(let i = 0; i < 64; i++){
         for(let j = 0; j < 64; j++){
             let colorIndex = drawStr.charAt(j*64 + i);
             c.fillStyle=palette[parseInt(colorIndex,16)];
+            c.strokeStyle='rgba(0,0,0,0)';
             if(colorIndex == 'b'){
                 c.fillStyle="#FFF";
+                c.strokeStyle="#FFF";
             }
-            c.fillRect(i*10, j*10, 10, 10);
+            c.fillRect(i*w, j*w, w, w);
         }
     }
 }
